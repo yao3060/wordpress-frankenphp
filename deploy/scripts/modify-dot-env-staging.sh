610b96f4342ab8c79ba452151b1e27e0e5abd2b2
Hai NGUYEN HOANG <hai.nguyen@it-consultis.com>
2019-01-17 07:29:06 +0000

    Update deploy/scripts/modify-dot-env-staging.sh, .env.default, .gitlab-ci.yml files

--- deploy/scripts/modify-dot-env-staging.sh
+++ deploy/scripts/modify-dot-env-staging.sh
@@ -4,11 +4,6 @@ set -aeuo pipefail
 sed -i "/ENVIRONMENT=/c ENVIRONMENT=staging" .env
 sed -i '/DOCKER_NETWORK_IPAM_SUBNET/s/^#\ //g' .env
 sed -i '/DOCKER_NETWORK_IPAM_GATEWAY/s/^#\ //g' .env
-sed -i "/REACT_HTTP_PORT=/c REACT_HTTP_PORT=7002" .env
-sed -i "/LARAVEL_HTTP_PORT=/c LARAVEL_HTTP_PORT=7003" .env
-sed -i "/DRUPAL_HTTP_PORT=/c LARAVEL_HTTP_PORT=7004" .env
+sed -i "/WORDPRESS_WEB_PORT=/c WORDPRESS_WEB_PORT=9993" .env 
 sed -i "/APP_ENV=/c APP_ENV=staging" .env
-sed -i "/APP_URL=/c APP_URL=https://base.it-consultis.net" .env
-sed -i "/API_URL=/c API_URL=https://base-api.it-consultis.net" .env
-sed -i "/ADMIN_BACKEND_DOMAIN=/c ADMIN_BACKEND_DOMAIN=base-admin.it-consultis.net" .env
-sed -i "/ADMIN_SECURE=/c ADMIN_SECURE=true" .env
+sed -i "/WORDPRESS_SITE_URL=/c WORDPRESS_SITE_URL=https://wordpress-demo.it-consultis.net" .env 
